3d1d50e2766f99be2d31eb6c8c71173ebb3cee26
Mizer33 <156226740+Mizer33@users.noreply.github.com>
2024-05-21 21:00:45 +0000

    Terminado

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-# Importar las bibliotecas necesarias
+
 from dotenv import load_dotenv
 import os
 import spotipy
@@ -6,7 +6,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
 # Configurar las credenciales para utilizar la API de Spotify
@@ -26,13 +25,14 @@ def obtener_top_tracks_con_duracion(artista_id):
 # Función para obtener el ID del artista y el top 10 de canciones
 artista_id, artista_nombre = buscar_id_artista("canserbero")  # Cambia "Queen" por el nombre del artista que desees
 top_canciones = obtener_top_tracks_con_duracion(artista_id)
-print(artista_id)
+
 # Crear un DataFrame con los datos obtenidos
 df_top_tracks = pd.DataFrame(top_canciones, columns=['Nombre Canción', 'Popularidad', 'Duración (ms)'])
 
 # Convertir la duración de milisegundos a minutos para mejor comprensión
 df_top_tracks['Duración (min)'] = df_top_tracks['Duración (ms)'] / 60000
 print(df_top_tracks)
+
 # Graficar un scatter plot para analizar la relación entre duración y popularidad
 plt.figure(figsize=(10, 6))
 plt.scatter(df_top_tracks['Duración (min)'], df_top_tracks['Popularidad'], color='blue')
@@ -41,7 +41,9 @@ plt.xlabel('Duración (minutos)')
 plt.ylabel('Popularidad')
 plt.grid(True)
 
-print("Mostrando gráfico...")
+# Guardar el gráfico como imagen
+plt.savefig('relacion_duracion_popularidad.png')
+
+print("La grafica esta en el archivo relacion_duracion ")
 plt.show()
-print("Gráfico mostrado.")
 
--- src/app2.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Convertir la lista de top tracks en un DataFrame
-df_top_tracks = pd.DataFrame(top_canciones, columns=['Nombre Canción', 'Popularidad', 'Álbum'])
-print(df_top_tracks)
-
-
